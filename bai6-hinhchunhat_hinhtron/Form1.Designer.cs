819447bc404af56709cebb4e767c99cb9b78215e
SPC <SPC@34-LAB402>
2025-02-28 09:00:36 +0700

    Add project files.

---
+++ bai6-hinhchunhat_hinhtron/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace bai6_hinhchunhat_hinhtron
+{
+    partial class tbnso2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtso1 = new System.Windows.Forms.TextBox();
+            this.txtso2 = new System.Windows.Forms.TextBox();
+            this.btnso1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labso1 = new System.Windows.Forms.Label();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtso3 = new System.Windows.Forms.TextBox();
+            this.btnso2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lab3 = new System.Windows.Forms.Label();
+            this.lab4 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "tính hình chứ nhật ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "chiều dài";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "chiều rộng";
+            // 
+            // txtso1
+            // 
+            this.txtso1.Location = new System.Drawing.Point(127, 67);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(148, 30);
+            this.txtso1.TabIndex = 2;
+            // 
+            // txtso2
+            // 
+            this.txtso2.Location = new System.Drawing.Point(127, 126);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(148, 30);
+            this.txtso2.TabIndex = 2;
+            // 
+            // btnso1
+            // 
+            this.btnso1.Location = new System.Drawing.Point(127, 179);
+            this.btnso1.Name = "btnso1";
+            this.btnso1.Size = new System.Drawing.Size(148, 32);
+            this.btnso1.TabIndex = 3;
+            this.btnso1.Text = "tính";
+            this.btnso1.UseVisualStyleBackColor = true;
+            this.btnso1.Click += new System.EventHandler(this.btnso1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 4;
+            // 
+            // labso1
+            // 
+            this.labso1.AutoSize = true;
+            this.labso1.Location = new System.Drawing.Point(51, 283);
+            this.labso1.Name = "labso1";
+            this.labso1.Size = new System.Drawing.Size(0, 25);
+            this.labso1.TabIndex = 5;
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Location = new System.Drawing.Point(18, 258);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(90, 25);
+            this.lab1.TabIndex = 6;
+            this.lab1.Text = "diện tích:";
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Location = new System.Drawing.Point(22, 308);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(69, 25);
+            this.lab2.TabIndex = 6;
+            this.lab2.Text = "chu vi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "bán kính";
+            // 
+            // txtso3
+            // 
+            this.txtso3.Location = new System.Drawing.Point(517, 72);
+            this.txtso3.Name = "txtso3";
+            this.txtso3.Size = new System.Drawing.Size(148, 30);
+            this.txtso3.TabIndex = 2;
+            // 
+            // btnso2
+            // 
+            this.btnso2.Location = new System.Drawing.Point(517, 124);
+            this.btnso2.Name = "btnso2";
+            this.btnso2.Size = new System.Drawing.Size(148, 32);
+            this.btnso2.TabIndex = 3;
+            this.btnso2.Text = "tính";
+            this.btnso2.UseVisualStyleBackColor = true;
+            this.btnso2.Click += new System.EventHandler(this.btnso2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(412, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 25);
+            this.label7.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(441, 288);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 25);
+            this.label8.TabIndex = 5;
+            // 
+            // lab3
+            // 
+            this.lab3.AutoSize = true;
+            this.lab3.Location = new System.Drawing.Point(408, 263);
+            this.lab3.Name = "lab3";
+            this.lab3.Size = new System.Drawing.Size(90, 25);
+            this.lab3.TabIndex = 6;
+            this.lab3.Text = "diện tích:";
+            // 
+            // lab4
+            // 
+            this.lab4.AutoSize = true;
+            this.lab4.Location = new System.Drawing.Point(412, 313);
+            this.lab4.Name = "lab4";
+            this.lab4.Size = new System.Drawing.Size(69, 25);
+            this.lab4.TabIndex = 6;
+            this.lab4.Text = "chu vi:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(495, 25);
+            this.label11.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(123, 25);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "tính hình tròn";
+            // 
+            // tbnso2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1589, 852);
+            this.Controls.Add(this.lab4);
+            this.Controls.Add(this.lab2);
+            this.Controls.Add(this.lab3);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labso1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnso2);
+            this.Controls.Add(this.btnso1);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.txtso3);
+            this.Controls.Add(this.txtso1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "tbnso2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtso1;
+        private System.Windows.Forms.TextBox txtso2;
+        private System.Windows.Forms.Button btnso1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labso1;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtso3;
+        private System.Windows.Forms.Button btnso2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lab3;
+        private System.Windows.Forms.Label lab4;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ bai6-hinhchunhat_hinhtron/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai6_hinhchunhat_hinhtron
+{
+    public partial class tbnso2 : Form
+    {
+        public tbnso2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnso1_Click(object sender, EventArgs e)
+        {
+            int cdai= Convert.ToInt32(txtso1.Text);
+            int crong= Convert.ToInt32(txtso2.Text);
+            int dt, chuv;
+            dt = cdai * crong;
+            chuv = (crong + dt)*2;
+            lab1.Text = ("diện tích: " + dt).ToString();
+            lab2.Text = ("chu vi: " + chuv).ToString();
+
+        }
+
+        private void btnso2_Click(object sender, EventArgs e)
+        {
+            int bk= Convert.ToInt32(txtso3.Text);
+            double dtht, chuvht;
+            dtht = 3.14 * (bk * bk);
+            chuvht = (bk * 2) * 3.14;
+            lab3.Text = ("diện tích: " + dtht).ToString();
+            lab4.Text = ("chu vi: " + chuvht).ToString();
+
+        }
+    }
+}
